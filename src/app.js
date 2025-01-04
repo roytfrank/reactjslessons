69241205469ccc728007214c90fe94a4a1d1499b
Frankline Ako <134519512+roytfrank@users.noreply.github.com>
2025-01-04 14:07:45 -0500

    Complete setup for crudapp

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-class Cars {}
+import React, {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+
+const App = function() {
+    return <h1>This is react rendering as it should be</h1>
+}
+
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
